14038bba3af68e4400c52b06ac68e50381e681d8
beginninggrace <151606621+beginninggrace@users.noreply.github.com>
2024-01-14 22:57:02 +0900

    Delete testtest2 directory

--- testtest2/src/SuperCar.java
+++
@@ -1,8 +0,0 @@
-public class SuperCar extends Car {
-    public long price;
-
-    SuperCar(String brand, double topSpeed, String name, int wheel, String color, long price) {
-        super(brand, topSpeed, name, wheel, color);
-        this.price = price;
-    }
-}
